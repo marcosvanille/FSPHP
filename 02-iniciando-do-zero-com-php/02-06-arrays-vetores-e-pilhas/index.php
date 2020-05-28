4c5fde6d5472d83fbb8086270c9dce49fff0f503
Your Name <marcosvanillle@gmail.com>
2020-05-28 20:45:43 -0300

    [ADD]projeto fsphp

---
+++ 02-iniciando-do-zero-com-php/02-06-arrays-vetores-e-pilhas/index.php
@@ -0,0 +1,31 @@
+<?php
+require __DIR__ . '/../../fullstackphp/fsphp.php';
+fullStackPHPClassName("02.06 - Arrays, vetores e pilhas");
+
+/**
+ * [ arrays ] https://php.net/manual/pt_BR/language.types.array.php
+ */
+fullStackPHPClassSession("index array", __LINE__);
+
+$arrA = array(1,2,3);
+
+var_dump($arrA);
+var_dump($arrA);
+
+/**
+ * [ associative array ] "key" => "value"
+ */
+fullStackPHPClassSession("associative array", __LINE__);
+
+
+/**
+ * [ multidimensional array ] "key" => ["key" => "value"]
+ */
+fullStackPHPClassSession("multidimensional array", __LINE__);
+
+
+/**
+ * [ array access ] foreach(array as item) || foreach(array as key => value)
+ */
+fullStackPHPClassSession("array access", __LINE__);
+
